77a6cd9e8317d2bb0ff75f735c5797717833d728
NathanPrez <nathanpreston@aol.com>
2021-11-24 17:57:06 +0000

    Admin Profile created

---
+++ database/seeders/AdminProfileTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AdminProfileTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $up = new UserProfile;
+        $up->user_id = 1;
+        $up->save();
+    }
+}
