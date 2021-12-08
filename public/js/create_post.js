a1b2d5e9b45cc6e39669d9ddb6c0f6f869daa920
NathanPrez <nathanpreston@aol.com>
2021-12-08 13:06:15 +0000

    Created uplaod page

--- public/js/create_post.js
+++ public/js/create_post.js
@@ -1,3 +1,7 @@
-if(document.forms["post-form"]["type"].value = "shot") {
-    document.getElementById('image-upload').classList.remove("hidden");
+function checkType(){
+    if(document.forms["post-form"]["type"].value = "shot") {
+        document.getElementById('image-upload').classList.remove("hide");
+    } else { 
+        document.getElementById('image-upload').classList.add("hide");
+    }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,7 +6,7 @@
         <div class="row space-bottom">
             <div class="col-md-2 mx-auto">Post Type:</div>
             <div class="col-md-8 content-div mx-auto">        
-                <select name="type">
+                <select id="type" name="type" onchange="checkType()">
                     <option value="shot">Shot</option>
                     <option value="chat">Chat</option>
                     <option value="meet">Meet</option>
@@ -20,21 +20,46 @@
             <div class="col-md-8 content-div mx-auto">        
                 <textarea name="body"></textarea>
             </div>
-
         </div>
 
-        <div id="image-upload" class="row hidden">
+        <div id="image-upload" class="row">
             <div class="col-md-2 mx-auto">Image:</div>
             <div class="col-md-8 content-div mx-auto">        
                 <textarea name="body"></textarea>
             </div>
         </div>
         
-        <input class="button--submit" type="submit">
+        <div class="row">
+            <div class="col-md-2 mx-auto"></div>
+            <div class="col-md-8 content-div mx-auto">        
+                <input type="submit">
+            </div>
+        </div>
     </form>
 @endsection
 
 
 @section('js')
-    <script src="{{ URL::asset('js/create_post.js') }}"></script>
+    <script>
+
+        function hide(name) {
+            document.getElementById(name).classList.add("hide");
+        }
+
+        function show(name) {
+            var elem = document.getElementById(name).classList;
+            if (elem.contains("hide")) {
+                document.getElementById(name).classList.remove("hide");
+            }
+        }
+
+        function checkType(){
+            console.log(document.getElementById("type").value);
+            if(document.forms["post-form"]["type"].value == "shot") {
+                show("image-upload");
+            } else { 
+                hide("image-upload");
+            }
+        }
+    </script>
 @endsection
